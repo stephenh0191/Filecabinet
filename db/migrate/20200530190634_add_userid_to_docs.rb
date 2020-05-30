875bc91c1c3d2156064e9f3d8ac6c1fa2ae94797
stephenh0191 <stephenhughes0191@gmail.com>
2020-05-30 20:16:40 +0100

    Association betwween user and fils

---
+++ db/migrate/20200530190634_add_userid_to_docs.rb
@@ -0,0 +1,5 @@
+class AddUseridToDocs < ActiveRecord::Migration[6.0]
+  def change
+    add_column :docs, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_190022) do
+ActiveRecord::Schema.define(version: 2020_05_30_190634) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "models", force: :cascade do |t|
